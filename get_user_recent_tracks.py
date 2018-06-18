a066c256dec0c737177c2ceb3a8f8535dd373948
Nikhil Ranjan <nikhilranjan7@gmail.com>
2018-06-18 19:42:08 +0800

    Change indentation

--- get_user_recent_tracks.py
+++ get_user_recent_tracks.py
@@ -14,12 +14,12 @@ def timeconv(time):
 
 # You have to have your own unique two values for API_KEY and API_SECRET
 # Obtain yours from https://www.last.fm/api/account/create for Last.fm
-API_KEY = "0f37a485b0270939e6ecb661d7895601"
-API_SECRET = "edf93985417e0f9093e08feae3cf6f80"
+API_KEY = ""
+API_SECRET = ""
 
 # In order to perform a write operation you need to authenticate yourself
 username = "nikhilranjan"
-password_hash = pylast.md5("davincifund77.A")
+password_hash = pylast.md5("")
 
 network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET,
                                username=username, password_hash=password_hash)
